a30f06c4e61e7c52d039175d84b551d679d774bf
Oluwasegun Adedeji <64655766+Hephzihub@users.noreply.github.com>
2022-03-09 12:16:55 +0100

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,10 @@
+# Oluwasegun Adedeji
+# Fun Fact: I love to see things work
+
+print(" OOO     A  ")
+print("O   O   A A ")
+print("O   O  A   A")
+print("O   O  AAAAA")
+print("O   O  A   A")
+print("O   O  A   A")
+print(" OOO   A   A")
